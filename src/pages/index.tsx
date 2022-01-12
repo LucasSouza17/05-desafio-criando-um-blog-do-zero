9b19684b6d1cb18690c28d26fd4681e94290ad5b
Lucas Fernando de Souza <lucasnando800@hotmail.com>
2022-01-11 22:37:36 -0300

    ajuste tamanho da tela e estilização da lista de posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 import Head from 'next/head';
 
 import { getPrismicClient } from '../services/prismic';
+
+import { FiCalendar, FiUser } from 'react-icons/fi'
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -31,6 +34,77 @@ export default function Home() {
       <Head>
         <title>Home | spacetraveling</title>
       </Head>
+
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          <Link href={`/post/slug`}>
+            <a>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div className={styles.metadata}>
+                <time>
+                  <FiCalendar color="#BBBBBB" />
+                  <span>19 Abr 2021</span>
+                </time>
+                <div className={styles.author}>
+                  <FiUser color="#BBBBBB" />
+                  <span>Danilo Vieira</span>
+                </div>
+              </div>
+            </a>
+          </Link>
+
+          <Link href={`/post/slug`}>
+            <a>
+              <strong>Criando um app CRA do zero</strong>
+              <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App</p>
+              <div className={styles.metadata}>
+                <time>
+                  <FiCalendar color="#BBBBBB" />
+                  <span>19 Abr 2021</span>
+                </time>
+                <div className={styles.author}>
+                  <FiUser color="#BBBBBB" />
+                  <span>Danilo Vieira</span>
+                </div>
+              </div>
+            </a>
+          </Link>
+          <Link href={`/post/slug`}>
+            <a>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div className={styles.metadata}>
+                <time>
+                  <FiCalendar color="#BBBBBB" />
+                  <span>19 Abr 2021</span>
+                </time>
+                <div className={styles.author}>
+                  <FiUser color="#BBBBBB" />
+                  <span>Danilo Vieira</span>
+                </div>
+              </div>
+            </a>
+          </Link>
+
+          <Link href={`/post/slug`}>
+            <a>
+              <strong>Criando um app CRA do zero</strong>
+              <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App</p>
+              <div className={styles.metadata}>
+                <time>
+                  <FiCalendar color="#BBBBBB" />
+                  <span>19 Abr 2021</span>
+                </time>
+                <div className={styles.author}>
+                  <FiUser color="#BBBBBB" />
+                  <span>Danilo Vieira</span>
+                </div>
+              </div>
+            </a>
+          </Link>
+        </div>
+      </main>
     </>
   );
 }
