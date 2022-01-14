b0060d0e13d439ea7f35d0af665fa28a5b110d8a
Lucas Fernando de Souza <lucasnando800@hotmail.com>
2022-01-13 22:45:12 -0300

    ajuste header (voltar para home quando clicar na logo)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
+import Link from 'next/link'
+
 import styles from './header.module.scss';
 
 export default function Header() {
   return (
     <header className={styles.headerContainer}>
-      <div className={styles.headerContent}>
+      <Link href="/">
+      <a className={styles.headerContent}>
         <img src="/images/logo.svg" alt="logo" />
-      </div>
+      </a>
+      </Link>
     </header>
   );
 }
